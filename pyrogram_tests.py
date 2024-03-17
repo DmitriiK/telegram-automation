ecfb7bad6a0a72e5bcad09fdcf462f831ad35491
DmitriiK <dklmn@mail.ru>
2024-03-17 18:14:08 +0300

    pirogram functions to retrieve reactions

--- pyrogram_tests.py
+++ pyrogram_tests.py
@@ -1,17 +1,28 @@
 from datetime import datetime
 import asyncio
 import os
+from typing import List
 
-from pyrogram import Client, types, filters
 from dotenv import load_dotenv
+
+from pyrogram import Client
+from pyrogram.raw.functions.messages import GetMessagesReactions
 
 load_dotenv()
 api_id = os.getenv('TELEGRAM_API_ID')
 api_hash = os.getenv('TELEGRAM_API_HASH')
-PHONE = '+79053862836'
+PHONE = os.getenv('PHONE')
+
 app = Client("my_account", api_id=api_id, api_hash=api_hash,  phone_number=PHONE)
 print(f'app started at {datetime.now()}')
- 
+
+
+async def get_reactions(chat_id: int, mess_ids: List[int]):
+    async with app:
+        gr = await app.resolve_peer(chat_id)
+        rr = await app.invoke(GetMessagesReactions(peer=gr, id=mess_ids))
+        print(rr)
+
 
 async def main():
     async with app:
@@ -21,4 +32,4 @@ async def main():
             print(post)
             await asyncio.sleep(1)
          
-app.run(main())
+app.run(get_reactions(chat_id=-1001688539638, mess_ids=[149230]))
