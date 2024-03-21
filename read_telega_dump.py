f998d08e784c0740cff3ac884e4e811e1911e472
DmitriiK <dklmn@mail.ru>
2024-03-21 11:15:41 +0300

    damp extraction - same columns as for api

--- read_telega_dump.py
+++ read_telega_dump.py
@@ -1,5 +1,5 @@
 
-
+# %%
 import pandas as pd
 import datetime
 import ijson
@@ -20,17 +20,17 @@ def telega_dump_to_pandas(dump_path: str) -> pd.DataFrame:
 
 
 def __extract_message_data(msg):
-    mess_data = {'id': int(msg.get('id', '')),
-                 'date': datetime.datetime.strptime(msg['date'], "%Y-%m-%dT%H:%M:%S"),
-                 'from': str(msg.get('from', '')) or '',
-                 'from_id': str(msg.get('from_id', '')) or '',
-                 'reply_to_message_id': str(msg.get('reply_to_message_id', '')) or ''
+    mess_data = {'msg_id': int(msg.get('id', '')),
+                 'msg_date': datetime.datetime.strptime(msg['date'], "%Y-%m-%dT%H:%M:%S"),
+                 'user_name': str(msg.get('from', '')) or '',
+                 'user_id': str(msg.get('from_id', '')) or '',
+                 'reply_to_msg_id': str(msg.get('reply_to_message_id', '')) or ''
                  }
     tes = msg['text_entities']
     msg_parts = [mp['text'] for mp in tes]
     text = ''.join(msg_parts)
-    mess_data["len"] = len(text)
-    mess_data["mess_text"] = text
+    mess_data["msg_len"] = len(text)
+    mess_data["msg_text"] = text
     return mess_data
 
 
@@ -39,5 +39,9 @@ def __extract_message_data(msg):
 if __name__ == "__main__":
     dump_path = r"D:\test_data\ChatExport_2024-03-14\result.json"
     df = telega_dump_to_pandas(dump_path=dump_path)
+    
+
+# %%
+df
 
 # %%
